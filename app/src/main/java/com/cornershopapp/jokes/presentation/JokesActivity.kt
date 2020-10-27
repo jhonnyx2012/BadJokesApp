e54ba00b7d54df2a597486dc58a71867451a16d8
Jhonny Barrios <jhonnyx2012@gmail.com>
2020-10-27 19:40:03 -0300

    improve jokes activity organization.

--- app/src/main/java/com/cornershopapp/jokes/presentation/JokesActivity.kt
+++ app/src/main/java/com/cornershopapp/jokes/presentation/JokesActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.viewModels
 import com.cornershopapp.core.presentation.*
 import com.cornershopapp.jokes.databinding.ActivityJokesBinding
 import com.cornershopapp.jokes.di.CommonInjection
+import com.cornershopapp.jokes.domain.model.Joke
 
 class JokesActivity : BaseActivity<ActivityJokesBinding>() {
 
@@ -18,44 +19,54 @@ class JokesActivity : BaseActivity<ActivityJokesBinding>() {
         viewModel.onStart()
     }
 
+    private fun observeStates() {
+        viewModel.stateLiveData.observe(this) {
+            when(it) {
+                is JokesState.Loading -> setLoading()
+                is JokesState.Loaded -> setLoaded(it.content)
+                is JokesState.Error -> setError(it.message)
+            }
+        }
+    }
+
     private fun setupListeners() {
         binding?.moreButton?.setOnClickListener { viewModel.onMoreButtonClicked() }
     }
 
-    private fun observeStates() {
-        viewModel.stateLiveData.observe(this) {
-            binding?.run {
-                when(it) {
-                    is JokesState.Loading -> {
-                        progressLoading.show()
-                        textFirstLine.hide()
-                        textSecondLine.hide()
-                        textCategory.hide()
-                        moreButton.disable()
-                    }
-                    is JokesState.Loaded -> {
-                        textFirstLine.text = it.content.firstLine
-                        textSecondLine.text = it.content.secondLine
-                        textCategory.text = it.content.category
-                        moreButton.text = "Give me more!"
-                        progressLoading.hide()
-                        textFirstLine.show()
-                        textSecondLine.show()
-                        textCategory.show()
-                        moreButton.enable()
-                    }
-                    is JokesState.Error -> {
-                        moreButton.text = "Retry"
-                        textFirstLine.text = CommonInjection.provideResourcesRepository().defaultErrorMessageString
-                        textSecondLine.text = it.message
-                        progressLoading.hide()
-                        textCategory.hide()
-                        textFirstLine.show()
-                        textSecondLine.show()
-                        moreButton.enable()
-                    }
-                }
-            }
+    private fun setError(message: String) {
+        binding?.run {
+            moreButton.text = "Retry"
+            textFirstLine.text = CommonInjection.provideResourcesRepository().defaultErrorMessageString
+            textSecondLine.text = message
+            progressLoading.hide()
+            textCategory.hide()
+            textFirstLine.show()
+            textSecondLine.show()
+            moreButton.enable()
+        }
+    }
+
+    private fun setLoaded(content: Joke) {
+        binding?.run {
+            textFirstLine.text = content.firstLine
+            textSecondLine.text = content.secondLine
+            textCategory.text = content.category
+            moreButton.text = "Give me more!"
+            progressLoading.hide()
+            textFirstLine.show()
+            textSecondLine.show()
+            textCategory.show()
+            moreButton.enable()
+        }
+    }
+
+    private fun setLoading() {
+        binding?.run {
+            progressLoading.show()
+            textFirstLine.hide()
+            textSecondLine.hide()
+            textCategory.hide()
+            moreButton.disable()
         }
     }
 }
